995a15928f7b63d42841edfe56a7d99322d45643
theGlvoes <yusfko@hotmail.com>
2019-10-09 11:55:55 +0800

    feat: 🎸 buffer
    完成buffer的换入换出函数、实现LRU策略、实现缓存的查询

--- storage/buffer.cpp
+++ storage/buffer.cpp
@@ -1,62 +1,130 @@
-#include "buffer.h"
+#include <assert.h>
 #include <string>
 using std::string;
 
-// ==================== extern api ====================
+#include "buffer.h"
+
+// ==================== init func ====================
 BufMgr::BufMgr(void)
 {
-    this->freeBlockHead = 0;
+    // 申请缓存区空间
+    bufpool.data = (char *)MemAllocNoThrow(BUFFER_NUM * BLOCK_SIZE);
+    bufpool.bufMeta = (BufMeta *)MemAllocNoThrow(BUFFER_NUM * sizeof(BufMeta));
+
+    if (bufpool.data && bufpool.bufMeta)
+    {
+        // 没有申请到内存空间
+        // TODO error log
+        assert(bufpool.data && bufpool.bufMeta);
+    }
     // 初始化缓冲区描述数据
+    this->freeBlockHead = 0;
     int i;
     for (i = 0; i < BUFFER_NUM - 1; i++)
     {
-        BufPool *bp_p = &(this->bufpool.bufMeta[i]);
-        bp_p->bufTd = i;
-        bp_p->pageNo = -1;
-        bp_p->visitTime = UTC();
+        BufMeta *bp_p = &(this->bufpool.bufMeta[i]);
+        bp_p->bufId = i;
+        bp_p->visitTime = UTCNowTimestamp();
         bp_p->fNext = i + 1;
         bp_p->bufMode = BM_free;
     }
 }
 
-long BufMgr::BufQuickLookup(BufTag btag)
+BufMgr::~BufMgr()
 {
-    return -1;
+    MemFree(this->bufpool.bufMeta);
+    MemFree(this->bufpool.data);
 }
 
-char *BufMgr::BufGetBlock(long bufId)
-{
-    return this->bufpool.data[bufId];
-}
-
-char *BufMgr::ReadBuffer(BUfTag btag)
+// ==================== public func ====================
+char *BufMgr::ReadBuffer(BufTag btag)
 {
     // step1 - 检查btag的合法性
     // BufCheckTag(btag);
 
-    long resId;
+    long buf_id;
     // step2 - 查询tag和buf id的映射转换
-    resId = this->BufQuickLookup(btag);
-    if (resId == -1)
+    buf_id = this->BufQuickLookup(btag);
+    if (buf_id == -1)
     {
         // 该缓存页不存在，发出load动作
-        resId = this->BufLoadPage(btag);
+        buf_id = this->BufLoadPage(btag);
     }
 
-    assert(resId >= 0 && resId < BUFFER_NUM);
+    assert(buf_id >= 0 && buf_id < BUFFER_NUM);
 
-    return this->BufGetBlock(resId);
+    return this->BufGetBlock(buf_id);
 }
 
-// ==================== internal func ====================
+// ==================== private func ====================
+// 从外存中加载一个页进入缓存中
+long BufMgr::BufLoadPage(BufTag btag)
+{
+    // 检查Buftag的合法性
+    // BufCheckTag(btag);
+
+    // 查看是否有空闲的缓存块
+    // 如果没有则调用淘汰算法
+
+    // TODO 加锁控制
+
+    if (this->freeBlockHead == BUF_FREE_LIST_EMPTY)
+    {
+        this->BufSchedule();
+    }
+
+    // 此时一定有空闲块可以使用
+    long newBufId = this->freeBlockHead;
+    assert(newBufId >= 0 && newBufId < BUFFER_NUM);
+
+    BufMeta *newbmeta = &(this->bufpool.bufMeta[newBufId]);
+    this->freeBlockHead = newbmeta->fNext == BUF_FREE_END ? BUF_FREE_LIST_EMPTY : newbmeta->fNext;
+
+    newbmeta->fNext = BUF_FREE_END;
+    newbmeta->bufMode = BM_ioProgress;
+    // TODO 释放锁
+
+    // TODO: 调用OS接口，从disk调用目标page
+    // char *page = load_disk_page(btag);
+    // char *blockStart = this->BufGetBlock(newBufId);
+    // memcpy(blockStart, page, BLOCK_SIZE * sizeof(char) );
+    newbmeta->bufMode = BM_isValid;
+    return newBufId;
+}
+
 // 这一部分主要是buffer的调度策略，LRU、FIFO等调页淘汰算法的实现
+// 暂时实现LRU算法
 void BufMgr::BufSchedule(void)
 {
     // 调用淘汰算法
-    // loserId = strategy();
-    // this->BufRemove(loserId);
+    long loserId = StrategyLRU();
+    this->BufRemove(loserId);
 }
 
+// LRU算法
+long BufMgr::StrategyLRU()
+{
+    long buf_id = -1;
+    long minVisit = UTCNowTimestamp();
+    int i;
+    for (i = 0; i < BUFFER_NUM; i++)
+    {
+        BufMeta bmeta = bufpool.bufMeta[i];
+        if (bmeta.bufMode != BM_isValid)
+        {
+            continue;
+        }
+        if (bmeta.visitTime < minVisit)
+        {
+            buf_id = i;
+            minVisit = bmeta.visitTime;
+        }
+    }
+    assert(buf_id >= 0 && buf_id < BUFFER_NUM);
+    return buf_id;
+}
+
+// 删除指定id的缓存块，将该块返回free list中
 bool BufMgr::BufRemove(long bufId)
 {
     if (bufId < 0 || bufId > BUFFER_NUM)
@@ -65,7 +133,7 @@ bool BufMgr::BufRemove(long bufId)
         return false;
     }
 
-    BUfMeta *bmeta = &(this->bufpool.bufMeta[index]);
+    BufMeta *bmeta = &(this->bufpool.bufMeta[bufId]);
 
     // 检查缓存块能否删除
     // BufMode不为空、不在io_progress等状态
@@ -79,8 +147,9 @@ bool BufMgr::BufRemove(long bufId)
     bmeta->bufMode = BM_free;
     bmeta->visitTime = 0;
 
-    if (this->freeBlockHead == BUF_FREE_END)
+    if (this->freeBlockHead == BUF_FREE_LIST_EMPTY)
     {
+        // 如果当前空闲list为空
         this->freeBlockHead = bufId;
         bmeta->fNext == BUF_FREE_END;
     }
@@ -94,36 +163,35 @@ bool BufMgr::BufRemove(long bufId)
     return true;
 }
 
-long BufMgr::BufLoadPage(BufTag btag)
+// 在所有状态为BM_isValid的缓存块中满足tag相等的缓存块
+// 如果找到这样的缓存块则返回该块的下标
+// 否则返回-1
+// TODO : opt 目前实现顺序查找 太low了
+long BufMgr::BufQuickLookup(BufTag btag)
 {
-    // 检查Buftag的合法性
-    // BufCheckTag(btag);
-
-    // 查看是否有空闲的缓存块
-    // 如果没有则调用淘汰算法
-
-    // TODO 加锁控制
-
-    if (this->freeBlockHead == BUF_FREE_END)
+    int i;
+    for (i = 0; i < BUFFER_NUM; i++)
     {
-        this->BufShedule();
+        BufMeta bmeta = bufpool.bufMeta[i];
+        if (bmeta.bufMode != BM_isValid)
+        {
+            continue;
+        }
+        if (CMPBufTag(btag, bmeta.bTag) == true)
+        {
+            return i;
+        }
     }
+    return -1;
+}
 
-    // 此时一定有空闲块可以使用
-    long newBufId = this->freeBlockHead;
-    assert(resId >= 0 && resId < BUFFER_NUM);
-
-    BufMeta *newbmeta = &(this->bufpool.bufMeta[newBufId]);
-    this->freeBlockHead = newbmeta->fNext;
-
-    newbmeta->fNext = BUF_FREE_END;
-    newbmeta->bufMode = BM_ioProgress;
-    // TODO 释放锁
-
-    // TODO: 调用OS接口，从disk调用目标page
-    // load_disk_page(btag);
-    newbmeta->bufMode = BM_isValid;
-    return newBufId;
+// 根据bufid返回缓存块的起始地址
+char *BufMgr::BufGetBlock(long bufId)
+{
+    assert(bufId >= 0 && bufId < BUFFER_NUM);
+    char *p;
+    p = this->bufpool.data + BLOCK_SIZE * bufId;
+    return p;
 }
 
 // ==================== OS call ====================
@@ -131,6 +199,33 @@ long BufMgr::BufLoadPage(BufTag btag)
 // 如追加、清空、删除等，查看PG的bufmgr
 // 以及一些内部的加锁（待定）
 
-BufMgr::BufMgr(){}
+// ==================== utils ====================
+long UTCNowTimestamp()
+{
+    return 0;
+}
 
-BufMgr::~BufMgr(){}
+// BufTag 的比较函数,BufTag的比较结果就只有相等以及不相等
+// 如果tag1等于tag2则返回true
+// 否则false
+bool CMPBufTag(const BufTag tag1, const BufTag tag2)
+{
+    return tag1.pageNo == tag2.pageNo;
+}
+
+// ==================== mem func ====================
+void *MemAllocNoThrow(std::size_t alloc_size)
+{
+    void *newMem = NULL;
+    newMem = std::malloc(alloc_size);
+    return newMem;
+}
+
+void MemFree(void *start)
+{
+    if (start == NULL)
+    {
+        return;
+    }
+    free(start);
+}
--- storage/buffer.h
+++ storage/buffer.h
@@ -3,9 +3,9 @@
 
 #include "config.h"
 
-#define BUF_FREE_END -1
+#define BUF_FREE_END -1	// free list内指针的空值
+#define BUF_FREE_LIST_EMPTY -1 // free list的头指针为空的情况
 // ==================== data structure ====================
-
 enum BufMode
 {
 	BM_free = 0,
@@ -13,45 +13,31 @@ enum BufMode
 	BM_isDirty,
 	BM_ioProgress,
 	BM_error
-}
+};
 
 // 缓冲块的标签，数据的描述符
 // 查询者自行组织buffer的tag，该tag作为查询的关键词
 struct BufTag
 {
 	long pageNo; //页号
-}
+};
 
 //缓冲块描述数据
-
 struct BufMeta
 {
 	BufTag bTag;	 // buf的数据tag
-	long bufTd;		 // buf的下标，[0, BUFFER_NUM)
+	long bufId;		 // buf的下标，[0, BUFFER_NUM)
 	long visitTime;  //访问时间
 	long fNext;		 // 空闲块的下一个指针，分配新块时需要
 	BufMode bufMode; // 是否为脏数据
-}
+};
 
 // 缓冲池数据
 struct BufPool
 {
-	char data[BUFFER_NUM][BLOCK_SIZE]; // 不采用二维数组的形式，直接分配blocksize * buffer_num 的大小，自行管理
-	BufMeta bufMeta[BUFFER_NUM];
-}
-=======
-struct BufMeta{
-	long pageNo;   //页号
-	long visitTime;//访问时间
-	bool isChanged;//是否更改
+	char *data;		  // [BUFFER_NUM][BLOCK_SIZE]; // 不采用二维数组的形式，直接分配blocksize * buffer_num 的大小，自行管理
+	BufMeta *bufMeta; // [BUFFER_NUM];
 };
-
-// 缓冲池数据
-struct BufPool{
-	char *data[BUFFER_NUM][PAGE_SIZE];
-	struct BufMeta bufMeta[BUFFER_NUM];
-};
-
 
 // ==================== manager function ====================
 // 缓冲池管理
@@ -61,13 +47,13 @@ public:
 	BufMgr();
 	~BufMgr();
 
-	void BufInit(void);
+	// void BufInit(void);
 
 	// buf 对外提供buf的读取函数
 	// 参数待定
 	// 基本思路：根据tag去返回一个块的数据
 	// tag与数据相关，确保唯一性
-	char* ReadBuffer(std::string tag);
+	char *ReadBuffer(BufTag tag);
 
 private:
 	BufPool bufpool;
@@ -77,7 +63,8 @@ private:
 	long BufLoadPage(BufTag btag);
 	bool BufRemove(long bufId);		  // bufId取值为[0, BUFFER_NUM)
 	long BufQuickLookup(BufTag btag); // 根据buf tag 迅速查询是否有缓存,如果不存则返回-1
-	char* BufGetBlock(long bufId);		// 根据下标获取缓存内容
+	char *BufGetBlock(long bufId);	// 根据下标获取缓存内容
+	long StrategyLRU();			// 淘汰算法LRU
 };
 
 #endif
