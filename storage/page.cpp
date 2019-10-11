1ec5717f0c6a926223314e5697dc34db13ab45ad
Abcner <2426192554@qq.com>
2019-10-12 00:11:33 +0800

    hrc

--- storage/page.cpp
+++ storage/page.cpp
@@ -1,15 +1,8 @@
 #include "page.h"
 #include "storage.h"
 
-PageMgr::PageMgr(){
-	
-}
-PageMgr::~PageMgr(){
-	
-}
 
-
-PageMgr::bool_empty(unsigned long bit_map,int position){
+ int bool_empty(unsigned long bit_map,int position){
 	unsigned long result = 0x00000001;
 	result = result<<(32-position);
 	result = result & bit_map;
@@ -22,12 +15,12 @@ PageMgr::bool_empty(unsigned long bit_map,int position){
 }
 
 
-PageMgr::setbitmap(unsigned long *bit_map,int position，int value){
+void setbitmap(unsigned long *bit_map,int position,int value){
 	if(value!=0&&value!=1){
 		printf("value的值不符合规则!/n");
 		return ;
 	}
-	if(value==bool_empty(bit_map,position)){
+	if(value==bool_empty(*bit_map,position)){
 		return;
 	}
 	unsigned long  result = 0x00000001;
@@ -37,17 +30,18 @@ PageMgr::setbitmap(unsigned long *bit_map,int position，int value){
 	}
 	else {
 		result = ~result;
-		*bit_map = *bitmap&result;
+		*bit_map = *bit_map&result;
 	}
 	
 }
 
 
-PageMgr::RequestPage(struct Storage *DB, long NeededPageNum){
+int RequestPage(struct Storage *DB, long NeededPageNum){
+	int flag = 0;
 	for(int i=0;i<DB->dbMeta.blockNum;i++){
 		int p_num = i/(8*sizeof(long));
 		int position = i- p_num*8*sizeof(long)+1;
-		int flag = 0;
+		
 		if(bool_empty(*(DB->freeSpaceBitMap)+p_num,position)==0){
 			int count = 0;
 			for(int j=i;j<DB->dbMeta.blockNum;j++){
@@ -65,7 +59,7 @@ PageMgr::RequestPage(struct Storage *DB, long NeededPageNum){
 				for(int j=0;j<NeededPageNum;j++){
 					p_num = (i+j)/(8*sizeof(long));
 					position =i+j- p_num*8*sizeof(long)+1;
-					setbitmap(*(*(DB->freeSpaceBitMap)+p_num),position,1);
+					setbitmap(DB->freeSpaceBitMap+p_num,position,1);
 				}
 				flag = 1;
 				return NewPages;
@@ -81,15 +75,16 @@ PageMgr::RequestPage(struct Storage *DB, long NeededPageNum){
 	}
 }
 
-PageMgr::recove_onepage(struct Storage *DB,int PageNo){
-	int p_num = PageNo/(8*sizeof(long);
+void recove_onepage(struct Storage *DB,int PageNo){
+	int p_num = PageNo/(8*sizeof(long));
 	int position = PageNo - p_num*8*sizeof(long)+1;
-	setbitmap(*(DB->freeSpaceBitMap)+p_num,position,0);
+	setbitmap(DB->freeSpaceBitMap+p_num,position,0);
 }
-PageMgr::recover_allpages(struct Storage *DB){
+
+void recover_allpages(struct Storage *DB){
 	for(int i= 0;i<DB->dbMeta.blockNum;i++){
 		int p_num = i/(8*sizeof(long));
 		int position = i- p_num*8*sizeof(long)+1;
-		setbitmap(*(DB->freeSpaceBitMap)+p_num,position,0);
+		setbitmap(DB->freeSpaceBitMap+p_num,position,0);
 	}
 }
