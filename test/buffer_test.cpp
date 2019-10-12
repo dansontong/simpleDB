f7b27fd83f1e49431ec8e6497155c429e915b3a1
theGlvoes <yusfko@hotmail.com>
2019-10-12 19:24:39 +0800

    feat: 🎸 buffer
    buffer模块重构（删去class结构）；完善日志输出&测试

---
+++ test/buffer_test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "buffer.h"
+
+using namespace std;
+
+int main()
+{
+    Buf_Init();
+    cout << "Buffer init success." << endl;
+
+    BufTag btag;
+    btag.pageNo = 101;
+    Buf_PrintInfo();
+
+    cout << "======= test alloc =======" << endl;
+    Buf_AllocBlock(btag);
+    Buf_PrintInfo();
+
+    cout << "======= test write =======" << endl;
+    char data[] = {"this is a test for buffer."};
+    cout << "data for write: " << data << endl;
+    Buf_WriteBuffer(btag, data);
+    Buf_PrintInfo();
+
+    cout << "======= test read =======" << endl;
+    // test read
+    char *buf_p = Buf_ReadBuffer(btag);
+    if (buf_p != NULL)
+        cout << "res: " << buf_p << endl;
+
+    cout << "======= test load page =======" << endl;
+    btag.pageNo = 102;
+    // test read
+    buf_p = Buf_ReadBuffer(btag);
+    Buf_PrintInfo();
+
+    if (buf_p != NULL)
+        cout << "res: " << buf_p << endl;
+    return 0;
+}
