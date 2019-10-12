7de9838243f72ee47670d3e400638facac602e1a
dansontong <1541842130@qq.com>
2019-10-12 23:25:45 +0800

    data_dict

--- storage/config.h
+++ storage/config.h
@@ -18,14 +18,17 @@ using namespace std;
 #define FILE_DATA_SIZE (1024*1024*1024)// 文件数据区大小1G，单位字节
 #define FILE_DATA_ADDR (BIT_MAP_ADDR + BIT_MAP_SIZE)         // 数据区起始位置
 #define BIT_MAP_SIZE (FILE_DATA_SIZE / BLOCK_SIZE / 8) // 位示图大小
-#define BIT_MAP_ADDR 1024               // 位示图起始位置   
+#define BIT_MAP_ADDR 1024               // 位示图起始位置 
+#define MAX_NAME_LENGTH 50              //表名,属性名等名字的最大长度
+#define MAX_ATTRIBUTE_NUM 20            //表中属性个数上限
 //TODO: 需测试确保dbMeta不会超出1024，否则位示图数据将会与之重叠，会出现未知错误
 
 // file 配置参数
-#define MAX_FILE_NUM 30      //最大文件数量
+#define MAX_FILE_NUM     100  //最大文件数量
 #define MAP_FILE         0
-#define NORMAL_FILE      1
-#define INDEX_FILE_BTREE 2
+#define TABLE_FILE      1    //表文件
+#define DATA_DICT_FILE   2    //数据字典文件
+#define INDEX_FILE_BTREE 3
 
 // page 配置参数
 #define PAGE_SIZE BLOCK_SIZE//页大小==文件块大小
@@ -36,5 +39,16 @@ using namespace std;
 // segment
 #define SEGMENT_NUM 5       //文件的段数
 
+typedef enum{
+	INT_TYPE = 0;
+	LONG_TYPE = 1;
+	FLOAT_TYPE = 2;
+	DOUBLE_TYPE = 3;
+	CHAR_TYPE = 4;
+	VARCHAR_TYPE = 5;
+	DATE_TYPE = 6;
+}DATA_TYPE;
+
+const static char dataTypeName[6][10]={"INT","LONG","FLOAT","DOUBLE","CHAR","VARCHAR","DATE"};
 
 #endif
--- storage/storage.cpp
+++ storage/storage.cpp
@@ -1,63 +1,80 @@
 #include <stdlib.h>
 #include "storage.h"
 
-void storege_initDB(struct Storage *storage, char *fileName){
+void storage_initDB(struct Storage *DB, char *fileName)
+{
 	FILE *dbFile = fopen(fileName, "rb");
-	if(dbFile == NULL){
+	if(dbFile == NULL)
+	{
 		printf("DataBase isn't exist, creating new dataBase.\n");
-		storege_createDbFile(fileName);
+		storage_createDbFile(fileName);
 		dbFile = fopen(fileName, "rb");
 	}
 
-	fread(&(storage->dbMeta), sizeof(struct DbMeta), 1, dbFile);
+	fread(&(DB->dbMeta), sizeof(struct DbMeta), 1, dbFile);
 	// 为空闲空间的位示图bitMap分配空间
-	storage->freeSpaceBitMap = (unsigned long *)malloc(storage->dbMeta.bitMapSize);
+	DB->freeSpaceBitMap = (unsigned long *)malloc(DB->dbMeta.bitMapSize);
 	//rewind(dbFile);
 	// 在文件中定位到bitMap开始的位置，令文件指针指向这里
-	fseek(dbFile, storage->dbMeta.bitMapAddr, SEEK_SET);
-	fread(storage->freeSpaceBitMap, storage->dbMeta.bitMapSize, 1, dbFile);// 从文件中读取bitMap的内容
+	fseek(dbFile, DB->dbMeta.bitMapAddr, SEEK_SET);
+	fread(DB->freeSpaceBitMap, DB->dbMeta.bitMapSize, 1, dbFile);// 从文件中读取bitMap的内容
 	fclose(dbFile);
+
+	//加载 数据字典
+	int fid = DB.DbMeta.dataDictFid;
+	if(fid < 0)
+	{
+		printf("dataDictionary file doesn't exist.\n");
+		fid = file_newFile(DB, DATA_DICT_FILE, 1);
+		DB.DbMeta.dataDictFid = fid;
+	}
+	
 }
 
-void storege_createDbFile(char *fileName){
-	struct Storage storage;
-	storage.dbMeta.blockSize = BLOCK_SIZE;
-	storage.dbMeta.blockNum = FILE_DATA_SIZE / BLOCK_SIZE; // 256*1024
-	storage.dbMeta.blockFree = storage.dbMeta.blockNum;
-	storage.dbMeta.bitMapSize = BIT_MAP_SIZE;
-	storage.dbMeta.bitMapAddr = BIT_MAP_ADDR;
-	storage.dbMeta.dataSize = FILE_DATA_SIZE;
-	storage.dbMeta.dataAddr = FILE_DATA_ADDR;
-	storage.dbMeta.currFileNum = 0;
-	//storage.dbMeta.fileMeta[MAX_FILE_NUM] = new FileMeta[MAX_FILE_NUM];
-	storage.dbMeta.fileMeta[0].id = 0;
-	strcpy(storage.dbMeta.fileMeta[0].name, "0");
-	storage.dbMeta.fileMeta[0].segNum = SEGMENT_NUM;
-	storage.dbMeta.fileMeta[0].state = 1;
-	memset(storage.dbMeta.fileMeta[0].segList, -1, sizeof(struct Segment) * SEGMENT_NUM);
+void storage_createDbFile(char *fileName)
+{
+	struct Storage DB;
+	DB.dbMeta.blockSize = BLOCK_SIZE;
+	DB.dbMeta.blockNum = FILE_DATA_SIZE / BLOCK_SIZE; // 256*1024
+	DB.dbMeta.blockFree = DB.dbMeta.blockNum;
+	DB.dbMeta.bitMapSize = BIT_MAP_SIZE;
+	DB.dbMeta.bitMapAddr = BIT_MAP_ADDR;
+	DB.dbMeta.dataSize = FILE_DATA_SIZE;
+	DB.dbMeta.dataAddr = FILE_DATA_ADDR;
+	DB.dbMeta.currFileNum = 0;
+	DB.dbMeta.dataDictFid = -1;
+	//DB.dbMeta.fileMeta[MAX_FILE_NUM] = new FileMeta[MAX_FILE_NUM];
+	DB.dbMeta.fileMeta[0].id = 0;
+	strcpy(DB.dbMeta.fileMeta[0].name, "0");
+	DB.dbMeta.fileMeta[0].segNum = SEGMENT_NUM;
+	DB.dbMeta.fileMeta[0].state = 1;
+	memset(DB.dbMeta.fileMeta[0].segList, -1, sizeof(struct Segment) * SEGMENT_NUM);
 
 	printf("dbMeta set done.\n");
 
 	// 为空闲空间映射表分配空间，所有的初始化为-1，表示空闲
-	storage.freeSpaceBitMap = (unsigned long *)malloc(storage.dbMeta.bitMapSize);
-	memset(storage.freeSpaceBitMap, -1, storage.dbMeta.bitMapSize);
+	DB.freeSpaceBitMap = (unsigned long *)malloc(DB.dbMeta.bitMapSize);
+	memset(DB.freeSpaceBitMap, -1, DB.dbMeta.bitMapSize);
 
 	FILE *dbFile = fopen(fileName, "wb");
 	// 把初始化的相关参数写到数据库文件头部
-	fwrite(&(storage.dbMeta), sizeof(struct DbMeta), 1, dbFile);
+	fwrite(&(DB.dbMeta), sizeof(struct DbMeta), 1, dbFile);
 	// 把bitMap写到数据库文件中
-	fseek(dbFile, storage.dbMeta.bitMapAddr, SEEK_SET);
-	fwrite(storage.freeSpaceBitMap, storage.dbMeta.bitMapSize, 1, dbFile);
+	fseek(dbFile, DB.dbMeta.bitMapAddr, SEEK_SET);
+	fwrite(DB.freeSpaceBitMap, DB.dbMeta.bitMapSize, 1, dbFile);
 
 	fclose(dbFile);
 }
 
-void storege_showDbInfo(struct Storage *storage){
+void storage_showDbInfo(struct Storage *DB){
 
 }
 
-int storege_memToDisk(struct Storage *storage){
+int storage_memToDisk(struct Storage *DB){
 
 	return 0;
 }
 
+bool tupleInsert(struct Storage *DB,int length, int FileID, char *str){
+
+}
