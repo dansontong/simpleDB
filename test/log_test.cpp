bcfd4927e956e70f5aabdc7daf643091b68665aa
dansontong <1541842130@qq.com>
2019-10-14 03:32:10 +0800

    long_size

--- test/log_test.cpp
+++ test/log_test.cpp
@@ -13,6 +13,8 @@ int main(int argc, char* argv[]) {
 	Log(ERROR, "logtemp:No.%d,char:%c",5, 'd');
 	Log(WARN, "logtemp:No.%s,char:%4f","hello", 0.1);
 	Log(INFO, "logtemp:No.%s,char:%4f","我是插进来的字符串", 0.1);
+	
+	log_init();
 }
 
 // 使用说明：
