90721f28f1cc1a37ce1f23098404c84e72df21f9
lianjinshily <lan_yang_chn@163.com>
2019-10-28 01:28:01 +0800

    alter index.cpp

--- index/index.h
+++ index/index.h
@@ -4,10 +4,10 @@
 #include "database.h"
 
 
-
 typedef struct
 {
 	int key;        //索引的列值
+	int pos;
 	int posPage;    //列值对应的页号
     int posOffset;  //列值的偏移量
-}RecordOffset;      //索引的记录
+}TreeRecord;      //索引的记录
