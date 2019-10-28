d447119844b61bc5710a94165cdadfb472b43a2f
dansontong <1541842130@qq.com>
2019-10-28 08:47:20 +0800

    debug

--- core/database.h
+++ core/database.h
@@ -73,7 +73,7 @@ int database_memToDisk();
 // 记录相关
 void insertRecord(int dictID, char *str);
 void deleteRecord(int dictID, Record *record);
-Record searchRecord(char* tableName,char* attrName, char* attribute);
+Record* searchRecord(char* tableName,char* attrName, char* attribute);
 
 
 // 表相关
