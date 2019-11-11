1248fb50c3815c543227ef1bbbd18954fb1997c7
dansontong <1541842130@qq.com>
2019-11-11 15:24:39 +0800

    join debug

--- executor/tmptable.h
+++ executor/tmptable.h
@@ -8,6 +8,6 @@
 int create_tmptable(Table table_1);//根据表创建临时表
 void insert_onerecord(int dictID,char *record);//往临时表里插入一条数据
 
-void HashRelation(struct dbSysHead *head, Relation rl, int pub_attr, multimap<int, long> *m);
+void HashRelation(Relation rl, int pub_attr, multimap<int, long> *m);
 
 #endif
